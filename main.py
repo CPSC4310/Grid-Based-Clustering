3b2af90d7482847b4168867024e5c2cc7e7dd815
Micah Tigley <tigleym@gmail.com>
2017-11-21 22:26:43 -0700

    Function for partioning attribute values.

--- main.py
+++ main.py
@@ -10,11 +10,25 @@ file = './data/Iris_Data.csv'
 def partitionAttributes(values, partitionSize = 5):
     """Divide values equally according to the specified partition size."""
 
+    rangeDistance = (max(values) - min(values)) / partitionSize
+
+    return [(min(values) + ( x * rangeDistance)) for x in range(partitionSize)].append(max(values))
+
+def parseValue(value):
+    """Parses string values into floats, otherwise return origin string."""
+    
+    try:
+        float(value)
+        return float(value)
+    except:
+        return value
 
 ## Parse incoming data set into an array of dictionaries
 f = open(file, 'r')
 lines = f.read().split('\n')
-attributes = lines[0].split(',')
+
+attributes = lines[0].split(',')                    # List of data attributes.
+valuesPerAttr = { attr: [] for attr in attributes } # Dictionary of all values per data attribute.
 
 for idx, line in enumerate(lines):
     if idx > 0:
@@ -23,7 +37,10 @@ for idx, line in enumerate(lines):
 
         for _idx, attr in enumerate(attributes):
             if len(row) == len(attributes):
-                item[attr] = row[_idx]
+                value = parseValue(row[_idx])
+
+                item[attr] = value
+                valuesPerAttr[attr].append(value)
 
         # Only add items with data in it.
         if bool(item):
