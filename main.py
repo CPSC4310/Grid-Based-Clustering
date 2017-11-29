6fbb46f09ef39f0d8d02cce58c779d36f457a4d6
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2017-11-28 21:37:51 -0700

    - Generate grid outputs for all possible combinations in Iris_Data.csv (n choose 2).

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Grid import *
 from parseCSV import csvToDictArray
 import csv
+import os
 
 file = './data/Iris_Data.csv'
 
@@ -9,6 +10,8 @@ parsedData = csvToDictArray(file)
 data_set = parsedData[0]
 attributes = parsedData[1]
 valuesPerAttr = parsedData[2]
+min_den = 10
+gridSize = len(attributes)
 
 def partitionAttributes(values, partitionSize = 5):
     """Divide values equally according to the specified partition size."""
@@ -20,40 +23,50 @@ def partitionAttributes(values, partitionSize = 5):
 
     return ranges
 
-min_den = 10
-gridSize = len(attributes)
+def clusterTwoColumns(columnOneIdentifier, columnTwoIdentifier):
+    # Build a grid for clustering against "petal_length" vs. "petal_width"
+    xAxisRange = partitionAttributes(valuesPerAttr[columnOneIdentifier])
+    yAxisRange = partitionAttributes(valuesPerAttr[columnTwoIdentifier])
 
-# TODO: We do want to make this dynamic.
-# Build a grid for clustering against "petal_length" vs. "petal_width"
-xAxisRange = partitionAttributes(valuesPerAttr["sepal_length"])
-yAxisRange = partitionAttributes(valuesPerAttr["sepal_width"])
+    data = [ { columnOneIdentifier: item[columnOneIdentifier], columnTwoIdentifier: item[columnTwoIdentifier], "species": item["species"] } for item in data_set ]
 
-data_set = [ { "sepal_length": item["sepal_length"], "sepal_width": item["sepal_width"], "species?": item["species"] } for item in data_set ]
+    grid = Grid(gridSize, xAxisRange, yAxisRange)
+    grid.buildGrid(min_den)
+    grid.addPoints(data, columnOneIdentifier, columnTwoIdentifier)
 
-grid = Grid(gridSize, xAxisRange, yAxisRange)
-grid.buildGrid(min_den)
-grid.addPoints(data_set, "sepal_length", "sepal_width")
+    # Gather and sort dense cells.
+    grid.getDenseCells()
+    grid.sortDenseCells()
 
-# Gather and sort dense cells.
-grid.getDenseCells()
-grid.sortDenseCells()
+    # Build clusters.
+    clusters = grid.mergeCells()
+    clusters = grid.mergeUncertainCells()
 
-# Build clusters.
-clusters = grid.mergeCells()
-clusters = grid.mergeUncertainCells()
+    # Perform some black magic. See equivalent in commented code below:
+    data = [ item for key, cluster in clusters.items() for cell in cluster for item in cell.getCellItems() ]
+    # data = []
+    # for key, cluster in clusters.items():
+    #     for cell in cluster:
+    #         for item in cell.getCellItems():
+    #             data.append(item)
 
-# Perform some black magic. See equivalent in commented code below:
-data = [ item for key, cluster in clusters.items() for cell in cluster for item in cell.getCellItems() ]
-# data = []
-# for key, cluster in clusters.items():
-#     for cell in cluster:
-#         for item in cell.getCellItems():
-#             data.append(item)
+    columns = [columnOneIdentifier, columnTwoIdentifier, "species"]
 
-columns = ['sepal_width', 'sepal_length', 'species?']
+    outputFolder = "./output/"
 
-# Write clustered data to csv
-with open('test.csv', 'wb') as f:
-    dictWriter = csv.DictWriter(f, columns)
-    dictWriter.writeheader()
-    dictWriter.writerows(data)
+    # Create the output directory if it doesn't already exist
+    if not os.path.isdir(outputFolder):
+        os.mkdir(outputFolder, 0755)
+
+    # Write clustered data to csv
+    with open(outputFolder + columnOneIdentifier + "-VS-" + columnTwoIdentifier + "-Clusters.csv", 'wb') as f:
+        dictWriter = csv.DictWriter(f, columns)
+        dictWriter.writeheader()
+        dictWriter.writerows(data)
+
+# TODO: make this get the column identifiers from row 1 in the csv instead of hardcoded values
+columnIdentifiers = ["sepal_width", "sepal_length", "petal_length", "petal_width"]
+
+for i in range(len(columnIdentifiers)):
+    for j in range(i+1, len(columnIdentifiers)):
+        clusterTwoColumns(columnIdentifiers[i], columnIdentifiers[j])
